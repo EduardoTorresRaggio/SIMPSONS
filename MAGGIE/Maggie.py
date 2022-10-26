8430a930c24b99cd37eed2452c2817d83595288d
Edu <eduedem@gift.com>
2022-10-26 11:04:27 +0200

    26/10 11.04

--- MAGGIE/Maggie.py
+++ MAGGIE/Maggie.py
@@ -17,38 +17,26 @@ while True:
    frase_simpson:str = datos[0]['quote']
    autor:str = datos[0]['character']
    
+   
+   with open('General/general.csv', 'a', newline='') as csvfile:
+      spamwriter = csv.writer(csvfile)
+      spamwriter.writerow(frase_simpson)
+      #guardar la frase en csv de General
 
    if autor == 'Lisa':
     #guardar la frase en csv de Lisa
-    with open('lisa.csv', 'a', newline='') as csvfile:
-     spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    with open('Lisa/lisa.csv', 'a', newline='') as csvfile:
+     spamwriter = csv.writer(csvfile)
      spamwriter.writerow(frase_simpson)
-    print(frase_simpson)
-    print(autor)
     #guardar la imagen en carpeta de Lisa
     
-    time.sleep(3)
+    
 
    elif autor == 'Homer Simpson':
-    with open('Homer_Simpson.csv', 'a', newline='') as csvfile:
-     spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    with open('Homer/homer.csv', 'a', newline='') as csvfile:
+     spamwriter = csv.writer(csvfile)
      spamwriter.writerow(frase_simpson)
-    print(frase_simpson)
-    print(autor)
-    #guardar la frase en csv de Homer
+    #guardar la frase en csv de Homer 
+
     
-    time.sleep(3)
-  
-   else:
-    with open('General.csv', 'a', newline='') as csvfile:
-     spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-     spamwriter.writerow(frase_simpson)
-    print(frase_simpson)
-    print(autor)
-    print('Es otro')
-    #guardar la frase en csv de General
-    
-    time.sleep(3)
+   time.sleep(3)
